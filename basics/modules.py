743021c31071787af6666d0b9dd981e106b8eee1
EmreSimseek <emre0816s@gmail.com>
2024-09-25 18:18:10 +0300

    Create modules.py

---
+++ basics/modules.py
@@ -0,0 +1,22 @@
+#1 Importing Modules
+import math # Import All Module
+print(math.sqrt(16))
+
+from math import factorial # Importing a Specific Part from a Module
+print(factorial(5))
+
+import math as m # Renaming Modules
+print(m.pow(2,3))
+
+from math import *  #Importing All Content 
+print(sin(0))
+print(pi)
+
+#2 Creating Our Own Modules
+import functions
+print(functions.basic_function())
+
+
+#3 Modules Loading Order and Path
+import sys
+print(sys.path)
